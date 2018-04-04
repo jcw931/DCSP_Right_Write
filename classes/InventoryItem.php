9d62e128de079fe03f030ff3a024557cedbbd69f
hunterrtyson <31674795+hunterrtyson@users.noreply.github.com>
2018-04-04 13:12:18 -0500

    Updated InventoryItem classes
    Implemented Pen, WoodPen, and MechPen classes. Removed Crayon class.

--- classes/InventoryItem.php
+++ classes/InventoryItem.php
@@ -1,9 +1,8 @@
 <?php
-
-/**********************************************************
-*  This file includes the inventory item
-*
-**********************************************************/
+/**************************************************************
+*  This file includes the InventoryItem class as well as all
+*  subclasses of inventory item (Pen, WoodPen, and MechPen).
+**************************************************************/
 class InventoryItem {
   private $id;
 	private $name;
@@ -13,10 +12,7 @@ class InventoryItem {
   private $itemColor;
   private $brand;
 	
-
-	
-	public function __construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand) {
-    
+	public function __construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand) {   
     $this->id = $newID;
     $this->name = $newName;
     $this->price = $newPrice;
@@ -24,7 +20,6 @@ class InventoryItem {
     $this->desc = $newDescription;
     $this->itemColor = $newItemColor;
     $this->brand = $newBrand;
-
   }
   
   public function setID($newID) {
@@ -35,7 +30,7 @@ class InventoryItem {
     $this->name = $newName;
   }
   
-  public function setPrice ($newPrice) {
+  public function setPrice($newPrice) {
     $this->price = $newPrice;
   }
   
@@ -51,7 +46,7 @@ class InventoryItem {
     $this->color = $newColor;
   }
   
-  public function setBrand ($newBrand) {
+  public function setBrand($newBrand) {
     $this->brand = $newBrand;
   }
   
@@ -84,24 +79,117 @@ class InventoryItem {
   }
 }
 
+// class for pen items
 class Pen extends InventoryItem {
-
+  private $tipType;
+  private $refill;
+  private $inkColor;
   
+  public function __construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand, $newTipType, $newRefill, $newInkColor) {
+    parent::__construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand);
+    $this->tipType = $newTipType;
+    $this->refill = $newRefill;
+    $this->inkColor = $newInkColor;
+  }
   
+  public function setTipType($newTip) {
+    $this->tipType = $newTip;
+  }
+  
+  public function setRefill($newRefill) {
+    $this->refill = $newRefill;
+  }
+  
+  public function setInkColor($newInk) {
+    $this->inkColor = $newInk;
+  }
+  
+  public function getTipType() {
+    return $this->tipType;
+  }
+  
+  public function getRefill() {
+    return $this->refill;
+  }
+  
+  public function getInkColor() {
+    return $this->inkColor;
+  }
 }
 
+// class for wooden pencil items
 class WoodPen extends InventoryItem {
+  private $number;
+  private $woodType;
+  private $leadColor;
   
+  public function __construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand, $newNumber, $newWood, $newLeadColor) {
+    parent::__construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand);
+    $this->number = $newNumber;
+    $this->woodType = $newWood;
+    $this->leadColor = $newLeadColor;
+  }
   
+  public function setNumber($newNumber) {
+    $this->number = $newNumber;
+  }
+  
+  public function setWoodType($newWood) {
+    $this->woodType = $newWood;
+  }
+  
+  public function setLeadColor($newLeadColor) {
+    $this->leadColor = $newLeadColor;
+  }
+  
+  public function getNumber() {
+    return $this->number;
+  }
+  
+  public function getWoodType() {
+    return $this->woodType;
+  }
+  
+  public function getLeadColor() {
+    return $this->leadColor;
+  }
 }
 
+// class for mechanical pencil items
 class MechPen extends InventoryItem {
+  private $leadWeight;
+  private $gripType;
+  private $leadColor;
   
+  public function __construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand, $newWeight, $newGrip, $newLeadColor) {
+    parent::__construct($newID, $newName, $newPrice, $newQuantity, $newDescription, $newItemColor, $newBrand);
+    $this->leadWeight = $newWeight;
+    $this->gripType = $newGrip;
+    $this->leadColor = $newLeadColor;
+  }
   
-}
-
-class Crayon extends InventoryItem {
+  public function setLeadWeight($newWeight) {
+    $this->leadWeight = $newWeight;
+  }
   
+  public function setGripType($newGrip) {
+    $this->gripType = $newGrip;
+  }
   
+  public function setLeadColor($newLeadColor) {
+    $this->leadColor = $newLeadColor;
+  }
+  
+  public function getLeadWeight() {
+    return $this->leadWeight;
+  }
+  
+  public function getGripType() {
+    return $this->gripType;
+  }
+  
+  public function getLeadColor() {
+    return $this->leadColor;
+  } 
 }
 ?>
