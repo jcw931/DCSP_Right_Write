a9acb22b8394584d28c731df90244c3a37195168
brittf <wbf49@msstate.edu>
2018-04-04 19:52:31 -0500

    initial commit
    created and upload files

---
+++ pages/HomePage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<body>
+
+
+<nav class="w3-bar w3-light-grey">
+	<a href="#home" class="w3-button w3-bar-item"><img src="TheRightWrite.png" width = "600", height = "75"></a>
+	<a href="#home" class="w3-button w3-bar-item">Home</a>
+	<a href="#account" class="w3-button w3-bar-item">Account</a>
+	<a href="#signout" class="w3-button w3-bar-item">Sign Out</a>
+	<div style="float:right" class="topnav">
+	  <input type="text" placeholder="Search..">
+	</div>
+</nav>
+
+
+<nav class="w3-bar w3-yellow">
+	<a href="#home" class="w3-button w3-bar-item">Home</a>
+	<a href="#account" class="w3-button w3-bar-item">Account</a>
+	<a href="#signout" class="w3-button w3-bar-item">Sign Out</a>
+	<a style="float:right" href="#cart" class="w3-button w3-bar-item"><img src="cart.png" width = "20", height = "20">Cart</a>
+</nav>
+
+
+<section class="w3-row-padding w3-center w3-green">
+  <article class="w3-third">
+    <p>Homepage Product</p>
+    <img src="image.jpg" alt="Random Name" width = "200", height = "200">
+  </article>
+</section>
+
+<section class="w3-row-padding w3-center w3-blue">
+  <article class="w3-third">
+    <p>Categories</p>
+    <img src="image.jpg" alt="Random Name" width = "200", height = "200">
+  </article>
+</section>
+
+<section class="w3-row-padding w3-center w3-purple">
+  <article class="w3-third">
+    <p>Best Sellers</p>
+    <img src="image.jpg" alt="Random Name" width = "200", height = "200">
+  </article>
+</section>
+
+<section class="w3-row-padding w3-center w3-red">
+  <article class="w3-third">
+    <p>Recommended</p>
+    <img src="image.jpg" alt="Random Name" width = "200", height = "200">
+  </article>
+</section>
+
+<section class="w3-row-padding w3-center w3-light-grey">
+  <article class="w3-third">
+    <p>New</p>
+    <img src="image.jpg" alt="Random Name" width = "200", height = "200">
+  </article>
+</section>
+
+
+
+
+
+
+</body>
+</html>
