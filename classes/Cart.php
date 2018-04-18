ae24a1fcfa1be269bdf23f9793632b3b9e939e30
hunterrtyson <31674795+hunterrtyson@users.noreply.github.com>
2018-04-18 16:52:37 -0500

    Updated Accounts, Cart, Inventory Items

--- classes/Cart.php
+++ classes/Cart.php
@@ -8,39 +8,56 @@
  * ************************************************************ */
 
 class Cart {
-    private $items;
-    private $customerID;
+    private $cartID; // varchar(32)
+    private $items = array(); // array of items in cart
+    private $customerID; // customer id of cart owner
 
-    public function __construct($newItems, $newCustomerID) {
-        $this->items = $newItems;
+    // constructor does not set items array; there should be no items in cart at creation
+    public function __construct($newCartID, $newCustomerID) {
+        $this->cartID = $newCartID;
         $this->customerID = $newCustomerID;
     }
 
+    // set cartID of Cart (string)
+    public function setCartID($newCartID) {
+        $this->cartID = $newCartID;
+    }
+
+    // ???? dont add multiple items at once
     public function setItems($newItems) {
         foreach ($newItems as $item) {
             $this->items = $item;
         }
     }
 
+    // set customerID of Cart (string)
     public function setCustomerID($newCustomerID) {
         $this->customerID = $newCustomerID;
     }
 
+    // returns cartID (string)
+    public function getCartID() {
+        return $this->cartID;
+    }
+
+    // returns array of Items in Cart (array)
     public function getItems() {
         return $this->items;
     }
 
+    // returns customerID (string)
     public function getCustomerID() {
         return $this->customerID;
     }
 
+    // add an item to the Cart (append to items array)
     public function addItem($newItem) {
-        $this->items = $newItem;
+        $this->items[] = $newItem;
     }
 
+    //  remove an item from Cart (remove from items array)
     public function removeItem($item) {
-        unset($this->items[array_search($item)]);
-        $this->items = array_values($this->items);
+        array_splice($this->items, array_search($item, $this->items), 1);
     }
 
     // finish this function this is how we place orders
@@ -48,4 +65,5 @@ class Cart {
         return;
     }
 }
+
 ?>
