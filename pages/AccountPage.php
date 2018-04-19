21c079e2729c0e87b575913d1bfcd76358444acb
brittf <wbf49@msstate.edu>
2018-04-18 19:18:30 -0500

    push to resolve an error

--- pages/AccountPage.php
+++ pages/AccountPage.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
+<title>The Right Write - Account</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -19,7 +19,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
     <a href="HomePage.php"><h3 class="w3-wide"><b><img src="TheRightWrite.png" width="210" height="150"></b></h3></a>
   </div>
-  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+  <div class="w3-padding-64 w3-large w3-text-black" style="font-weight:bold">
     <a href="#" class="w3-bar-item w3-button">Wooden Pencils</a>
     <a href="#" class="w3-bar-item w3-button">Mechanical Pencils</a>
     <a href="#" class="w3-bar-item w3-button">Pens</a>
@@ -38,24 +38,20 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <p class="w3-right">
 		<a href="HomePage.php">Home</a>
 		<a href="AccountPage.php">Account</a>
-		<a href="LoginPage.php">Login</a>
+		<?php
+			session_start();
+			if (isset($_SESSION['uname'])) {
+			echo '<a href="LogoutPage.php">Logout</a>';
+			}
+			else { // if no user is logged in
+			echo '<a href="LoginPage.php">Login</a>';
+			}
+		?>
 		<a href="CartPage.php">Cart (0)</a>
-      <div style="float:right" class="topnav">
-	  <input type="text" placeholder="Search..">
-	</div>
+		<input type="text" placeholder="Search..">
     </p>
   </header>
 
-  <!-- Image header -->
-  <div class="w3-display-container w3-container">
-    <img src="/w3images/jeans.jpg" alt="Jeans" style="width:100%">
-    <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
-      <h1 class="w3-jumbo w3-hide-small">New arrivals</h1>
-      <h1 class="w3-hide-large w3-hide-medium">New arrivals</h1>
-      <h1 class="w3-hide-small">COLLECTION 2016</h1>
-      <p><a href="#jeans" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
-    </div>
-  </div>
 
   <div class="w3-container w3-text-grey" id="jeans">
     <p>8 items</p>
@@ -119,33 +115,6 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
       </div>
     </div>
   </div>
-
-<script>
-// Accordion 
-function myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-// Click on the "Jeans" link on page load to open the accordion for demo purposes
-document.getElementById("myBtn").click();
-
-
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-</script>
 
 </body>
 </html>
--- pages/LoginPage.php
+++ pages/LoginPage.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS Template</title>
+<title>The Right Write - Login</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -11,7 +11,40 @@
 .w3-sidebar a {font-family: "Roboto", sans-serif}
 body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
 </style>
+
 <body class="w3-content" style="max-width:2400px">
+
+
+
+<?php
+	require_once 'databaseController.php';
+	if (isset($_SESSION['uname'])) {
+		goto_home();
+	}
+
+	else if ($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		echo 'One small step for man';
+		session_start();
+		echo 'One';
+		$_SESSION['uname'] = $_POST['username'];
+		echo 'Giant';
+		$_SESSION['pword'] = $_POST['password'];
+		echo 'Leap';
+		$_SESSION['type'] = check_credentials($_SESSION['uname'], $_SESSION['pword']);
+		echo 'For';
+
+		
+		if (($_SESSION['type'] == 'customer') or ($_SESSION['type'] == 'vendor') or ($_SESSION['type'] == 'admin')) {
+			goto_home();
+		}
+	}
+?>
+
+
+
+
+
 
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-bar-block w3-yellow w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
@@ -19,7 +52,7 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
     <a href="HomePage.php"><h3 class="w3-wide"><b><img src="TheRightWrite.png" width="210" height="150"></b></h3></a>
   </div>
-  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+  <div class="w3-padding-64 w3-large w3-text-black" style="font-weight:bold">
     <a href="#" class="w3-bar-item w3-button">Wooden Pencils</a>
     <a href="#" class="w3-bar-item w3-button">Mechanical Pencils</a>
     <a href="#" class="w3-bar-item w3-button">Pens</a>
@@ -38,114 +71,111 @@ body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
     <p class="w3-right">
 		<a href="HomePage.php">Home</a>
 		<a href="AccountPage.php">Account</a>
-		<a href="LoginPage.php">Login</a>
+		<?php
+			session_start();
+			if (isset($_SESSION['uname'])) {
+				echo '<a href="LogoutPage.php">Logout</a>';
+			}
+			else {
+				echo '<a href="LoginPage.php">Login</a>';
+			}
+		?>
 		<a href="CartPage.php">Cart (0)</a>
-      <div style="float:right" class="topnav">
-	  <input type="text" placeholder="Search..">
-	</div>
+		<input type="text" placeholder="Search..">
     </p>
   </header>
 
-  <!-- Image header -->
-  <div class="w3-display-container w3-container">
-    <img src="/w3images/jeans.jpg" alt="Jeans" style="width:100%">
-    <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
-      <h1 class="w3-jumbo w3-hide-small">New arrivals</h1>
-      <h1 class="w3-hide-large w3-hide-medium">New arrivals</h1>
-      <h1 class="w3-hide-small">COLLECTION 2016</h1>
-      <p><a href="#jeans" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
-    </div>
-  </div>
-
-  <div class="w3-container w3-text-grey" id="jeans">
-    <p>8 items</p>
-  </div>
-
   <!-- Product grid -->
   <div class="w3-row w3-grayscale">
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="/w3images/jeans1.jpg" style="width:100%">
-        <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
-      </div>
-      <div class="w3-container">
-        <img src="/w3images/jeans2.jpg" style="width:100%">
-        <p>Mega Ripped Jeans<br><b>$19.99</b></p>
-      </div>
-    </div>
+  
+	<form method="post" action="LoginPage.php">
+		<div class="w3-col l3 s6">
+			<div class="w3-container">
+				<p style="color: red">
+					<?php
+						if ($_SESSION['type'] == 'invalid')
+							echo 'Username or Password is incorrect.';
+					?>
+				</p>
+			</div>
+			<div class="w3-container">
+				<p>Username<br></p>
+			</div>
+			<div class="w3-container">
+				<input type="text" name="username" value="<?php echo $_POST['username'] ?>"> <br>
+			</div>
+			<div class="w3-container">
+				<p>Password<br></p>
+			</div>
+			<div class="w3-container">
+				<input type="password" name="password" value="<?php echo $_POST['password'] ?>"> <br>
+			</div>
+			<div class="w3-container">
+				<p><input type="submit" value="Login"></p>
+			</div>
+			<div class="w3-container">
+				<p>Don't have an account? <a href="CreateAccountPage.php">Create Account</a></p>
+			</div>
+		</div>
+	</form>
 
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="/w3images/jeans2.jpg" style="width:100%">
-          <span class="w3-tag w3-display-topleft">New</span>
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p>Mega Ripped Jeans<br><b>$19.99</b></p>
-      </div>
-      <div class="w3-container">
-        <img src="/w3images/jeans3.jpg" style="width:100%">
-        <p>Washed Skinny Jeans<br><b>$20.50</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="/w3images/jeans3.jpg" style="width:100%">
-        <p>Washed Skinny Jeans<br><b>$20.50</b></p>
-      </div>
-      <div class="w3-container">
-        <div class="w3-display-container">
-          <img src="/w3images/jeans4.jpg" style="width:100%">
-          <span class="w3-tag w3-display-topleft">Sale</span>
-          <div class="w3-display-middle w3-display-hover">
-            <button class="w3-button w3-black">Buy now <i class="fa fa-shopping-cart"></i></button>
-          </div>
-        </div>
-        <p>Vintage Skinny Jeans<br><b class="w3-text-red">$14.99</b></p>
-      </div>
-    </div>
-
-    <div class="w3-col l3 s6">
-      <div class="w3-container">
-        <img src="/w3images/jeans4.jpg" style="width:100%">
-        <p>Vintage Skinny Jeans<br><b>$14.99</b></p>
-      </div>
-      <div class="w3-container">
-        <img src="/w3images/jeans1.jpg" style="width:100%">
-        <p>Ripped Skinny Jeans<br><b>$24.99</b></p>
-      </div>
-    </div>
   </div>
-
-<script>
-// Accordion 
-function myAccFunc() {
-    var x = document.getElementById("demoAcc");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-// Click on the "Jeans" link on page load to open the accordion for demo purposes
-document.getElementById("myBtn").click();
-
-
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-</script>
+  
+  <?php
+	// Given a uname and pword: returns "invalid" if uname and pword do not match a database account;
+	// else returns "customer", "vendor", or "admin" based on the type of the user's account.
+	function check_credentials($uname, $pword) {
+		echo 'Hello';
+		require_once 'databaseController.php';
+		echo 'Some';
+		$answer = validCustomerUsername($un, $pw, $hostName, $database, $uname);
+		echo 'Body';
+		if ($answer) {
+			$psalt = "qm&h*" . $pword . "pg!@";
+			$token = hash('ripemd128', $psalt);
+			echo 'Once';
+			$result = allCustomerData($un, $pw, $hostName, $database, $uname);
+			echo 'Told';
+			if ($token == $result['password']) {
+				if ($result['customerID'][0] == 'c') {
+					$answer = 'customer';
+				}
+				else if ($result['customerID'][0] == 'v') {
+					$answer = 'vendor';
+				}
+				else if ($result['customerID'][0] == 'a') {
+					$answer = 'admin';
+				}
+				$answer = $result['customerID'][0];
+			}
+			else
+				$answer = 'invalid';
+		}
+		else {
+			$answer = 'invalid';
+		}
+		return $answer;
+	}
+	
+	function goto_home()
+	{
+		header("Location: HomePage.php");
+		exit;
+	}
+  ?>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
