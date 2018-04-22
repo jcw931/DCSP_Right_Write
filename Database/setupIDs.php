04b33569cb104dd4fbab491b6e6cda5b04a2197d
bnj111 <bnj111@msstate.edu>
2018-04-21 21:32:30 -0500

    creates setup id table (only need to run once)

---
+++ Database/setupIDs.php
@@ -0,0 +1,5 @@
+<?php
+require_once "./../Database/databaseController.php";
+require_once "./../Database/login.php";
+startIDTracker($un, $pw, $hostName, $database, 'c1000', 'v1000', 'a1000', 'cart1000', 'item1000', 'order1000');
+?>
